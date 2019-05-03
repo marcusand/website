ff70c7bbb47ba26559854b66ac26129a89376374
Marcus Schreiter <marcus.schreiter@meso.design>
2019-05-03 16:10:09 +0200

    add footer to index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Footer from '../components/footer';
+import '../styles/layout.scss';
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['marcusand', 'website', 'portfolio']} />
+    <Footer/>
   </Layout>
 );
 
